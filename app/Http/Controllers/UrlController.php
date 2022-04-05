ce426d74212df59fdbde1a6b9836fed5ba57abc1
saffq <krystian.stepien2>
2022-04-05 17:22:10 +0200

    zmiana tekstu

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -10,13 +10,13 @@ class UrlController extends Controller
 {
     public function jsonData(Request $request)
     {
-        $hostUrl = $request->getSchemeAndHttpHost();
-        $pdf = PDF::loadView('invoice', ["src" => $request->src, "colors" => $request->colors]);
-        Storage::put('public/pdf/invoice.pdf', $pdf->output());
 
+        $hostUrl = $request->getSchemeAndHttpHost();
+        $pdf = PDF::loadView('invoice', ["src" => $request->src, "src1" => $request->src1, "src2" => $request->src2, "src3" => $request->src3,"colors" => $request->colors]);
+        Storage::put('public/pdf/invoice.pdf', $pdf->output());
         $url = Storage::url('public/pdf/invoice.pdf');
         return response()->json(
-            ['src' => $hostUrl.$url ]);
+            ['src' => $hostUrl.$url]);
     }
 
 }
--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -35,14 +35,6 @@
             text-align: center;
 
         }
-
-        table {
-            border-collapse: collapse;
-            margin-top: 30px;
-            font-size: 10pt;
-            margin-right: 15px !important;
-        }
-
         #top {
             display: flex;
             justify-content: space-between;
@@ -56,26 +48,33 @@
     <div class="left" style="font-size: 8pt;">
         <table id="firstTable" style="border: 1px solid black; margin-bottom: 100px; ">';
           <tr>
-                <td><h1 style="font-size: 30pt; margin-left: 15px !important; margin-right: 15px !important;">SIMULATION</h1></td>
-              <td><p style="text-align: left;font-size: 6pt; overflow-wrap: break-word; margin-right: 15px !important;">Szanowny Kliencie, <br>
-                      Prosimy o dokładne sprawdzenie załączonej makiety pod kątem błędów, takich jak treść projektu i umiejscowienie,
-                      kolory, pisownia i tak dalej. Wszelkie rozbieżności powinny być wyraźnie zaznaczone.
-                      Nasza firma nie odpowiada za błędy, które nie zostały zaznaczone na makiecie.
-                  </p></td>
+                <td><h1 style="font-size: 30pt; margin-left: 15px !important; margin-right: 15px !important;">Visualisation</h1></td>
                 <td><p style="text-align: left;font-size: 6pt; overflow-wrap: break-word; margin-right: 15px !important;">Dear Customer, <br>
                         Please check the attached mockup carefully for any errors such as design content and placement,
                         colours, spelling and so on. Any discrepancies should be clearly marked.
                         Our company is not responsible for mistakes that were not marked on the mockup.
                     </p></td>
+              <td><p style="font-size: 6pt; overflow-wrap: break-word; margin-right: 15px !important; text-align: left;">Lieber Kunde, <br>
+                      Bitte überprüfen Sie das beigefügte Mockup sorgfältig auf Fehler wie Designinhalt und Platzierung,
+                      Farben, Rechtschreibung und so weiter. Etwaige Abweichungen sind deutlich zu kennzeichnen.
+                      Unser Unternehmen ist nicht verantwortlich für Fehler, die nicht auf dem Mockup gekennzeichnet wurden.
+                  </p></td>
             </tr>
          </table>
 </div>
-    <img src="{{$src}}" style="margin-left: 200px; margin-bottom: 100px; ">
-    <h1>Available colors:</h1>
-@foreach($colors as $c)
-    <div style="border-radius: 1px;display: inline-block; height: 20px; width: 50px; margin-right: 40px;  margin-top: 50px; border: 1px solid black; background-color: {{$c[1]}}; ">
-        <div style="text-align: left;font-size: 8pt; margin-top: 20px; margin-right: 10px;">{{$c[0]}}</div> </div>
-@endforeach
-
+    @if (@getimagesize($src))
+    <img src="{{$src}}" style="margin-right: 80px">
+    @endif
+    @if (@getimagesize($src1))
+    <img src="{{$src1}}">
+    @endif
+    <br>
+    @if (@getimagesize($src2))
+    <img src="{{$src2}}" style="margin-right: 80px">
+    @endif
+    @if (@getimagesize($src3))
+    <img src="{{$src3}}">
+    @endif
+</div>
 </body>
 </html>

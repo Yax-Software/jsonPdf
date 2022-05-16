df3de77f6902d5545087300e53d5a8718ddf0460
saffq <krystian.stepien2>
2022-05-16 10:50:32 +0200

    zmiana pdfa

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use PDF;
 use Illuminate\Support\Facades\Storage;
-
+use Illuminate\Support\Str;
 class UrlController extends Controller
 {
     public function jsonData(Request $request)
@@ -13,8 +13,9 @@ class UrlController extends Controller
 
         $hostUrl = $request->getSchemeAndHttpHost();
         $pdf = PDF::loadView('invoice', ["src" => $request->src, "src1" => $request->src1, "src2" => $request->src2, "src3" => $request->src3,"colors" => $request->colors]);
-        Storage::put('public/pdf/invoice.pdf', $pdf->output());
-        $url = Storage::url('public/pdf/invoice.pdf');
+        $random = Str::random(12);
+        Storage::put('public/pdf/invoice'.$random.'.pdf', $pdf->output());
+        $url = Storage::url('public/pdf/invoice'.$random.'.pdf');
         return response()->json(
             ['src' => $hostUrl.$url]);
     }
--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -1,12 +1,32 @@
-<!doctype html>
-<html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Invoice</title>
+        <title>VISUALISATION</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap');
+             .shirt
+        {
+            z-index: 3;
+            transform: scale(0.8);
+
+        }
+        .bg {
+            background-image: url("../test.png");
+            width: 100%;
+            height: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        body, html {
+            height: 100%;
+            margin: 0;
+        }
+
+    </style>
     <script>
         window.axios = require('axios');
 
@@ -20,61 +40,22 @@
             console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
         }
     </script>
-    <style>
-        th {
-            border: 1px black;
-            border-style: solid none;
-            text-align: center;
-        }
-
-        body {
-            font-family: 'Open Sans', sans-serif;
-        }
-
-        td {
-            text-align: center;
-
-        }
-        #top {
-            display: flex;
-            justify-content: space-between;
-        }
-
-    </style>
-</head>
+ </head>
 <body>
-<div id="top">
     @csrf
-    <div class="left" style="font-size: 8pt;">
-        <table id="firstTable" style="border: 1px solid black; margin-bottom: 100px; ">';
-          <tr>
-                <td><h1 style="font-size: 30pt; margin-left: 15px !important; margin-right: 15px !important;">Visualisation</h1></td>
-                <td><p style="text-align: left;font-size: 6pt; overflow-wrap: break-word; margin-right: 15px !important;">Dear Customer, <br>
-                        Please check the attached mockup carefully for any errors such as design content and placement,
-                        colours, spelling and so on. Any discrepancies should be clearly marked.
-                        Our company is not responsible for mistakes that were not marked on the mockup.
-                    </p></td>
-              <td><p style="font-size: 6pt; overflow-wrap: break-word; margin-right: 15px !important; text-align: left;">Lieber Kunde, <br>
-                      Bitte überprüfen Sie das beigefügte Mockup sorgfältig auf Fehler wie Designinhalt und Platzierung,
-                      Farben, Rechtschreibung und so weiter. Etwaige Abweichungen sind deutlich zu kennzeichnen.
-                      Unser Unternehmen ist nicht verantwortlich für Fehler, die nicht auf dem Mockup gekennzeichnet wurden.
-                  </p></td>
-            </tr>
-         </table>
-</div>
+<div class="bg" style="width: 100%;height: 100%;">
     @if (@getimagesize($src))
-    <img src="{{$src}}" style="margin-right: 80px">
+    <img class="shirt" src="{{$src}}" style="margin-top: 320px !important;margin-right: 80px !important;margin-left: 55px !important">
     @endif
     @if (@getimagesize($src1))
-    <img src="{{$src1}}">
+    <img class="shirt" src="{{$src1}}">
     @endif
     <br>
     @if (@getimagesize($src2))
-    <img src="{{$src2}}" style="margin-right: 80px">
+    <img class="shirt" src="{{$src2}}" style="margin-right: 80px !important; margin-left: 55px !important">
     @endif
     @if (@getimagesize($src3))
-    <img src="{{$src3}}">
+    <img class="shirt" src="{{$src3}}">
     @endif
 </div>
 </body>
-</html>
